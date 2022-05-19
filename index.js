0e6d7122f8fb5da15bf332a86adbbc691775917b
Philip Holtzman <phil.holtzman@gmail.com>
2022-05-19 15:15:15 +0300

    set everything up more or less, css is still jacked tho

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const DECISIONS = ["rock","paper","scissors"];
+
+let computerDecision = '';
+
+function computerPlay(){
+    computerDecision = Math.floor(Math.random() * 3)
+    console.log(DECISIONS[computerDecision]);
+}
+
+function playerTurn(){
+    return null;
+};
+
+
+function game(){
+    return null;
+};
+
+
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
