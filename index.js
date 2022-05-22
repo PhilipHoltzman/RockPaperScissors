01c9a6f5994fb63b5302c7e5918457208b85e7ae
Philip Holtzman <phil.holtzman@gmail.com>
2022-05-22 17:28:55 +0300

    done. fully funcional, not adding a play again button.. I am done with this rock paper scissors exercise....

--- index.js
+++ index.js
@@ -15,43 +15,42 @@ rockButton.addEventListener("click", rockChoice);
 
 function rockChoice(){
     game(0);
-
 };
 
 paperButton.addEventListener("click", paperChoice );
 
 function paperChoice(){
     game(1);
-
 };
 
 scissorsButton.addEventListener("click", scissorsChoice );
 
 function scissorsChoice(){
-
     game(2);
-
 };
 
 function computerPlay(){
     computerDecision = Math.floor(Math.random() * 3)
 };
 
-function playerSelection(){
-    computerDecision = Math.floor(Math.random() * 3)
-    
-};
-
-
 function scoreCheck(){
     updatePlayerScore(playerScore);
     updateComputerScore(computerScore);
     if(computerScore >= 5 || playerScore >= 5){
+        var elem = document.getElementById('winlose');
         if(playerScore > computerScore){
             console.log("PLAYER WON!")
+            elem.textContent = "CONGRATS YOU WON!!! COMPUTER LOSES!!!!"
+            document.getElementById('Rock').disabled = true;
+            document.getElementById('Paper').disabled = true;
+            document.getElementById('Scissors').disabled = true;
         }
         else{
             console.log("Computer won!")
+            elem.textContent = "YOU LOSE!!! COMPUTER WINS!!!!"
+            document.getElementById('Rock').disabled = true;
+            document.getElementById('Paper').disabled = true;
+            document.getElementById('Scissors').disabled = true;
         }
     }
     else{
@@ -71,87 +70,58 @@ function updateComputerScore(computerScore){
 
 function game(playerchoice){
     computerPlay();
-    //playerSelection();
-    
+    console.log("Computer chose: " + computerDecision)
 
     // if player chooses Rock
     if(playerchoice == 0 && computerDecision == 1){
-        
         computerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     if(playerchoice == 0 && computerDecision == 2){
-        
         playerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     if(playerchoice == 0 && computerDecision == 0){
-        
         console.log("Tie");
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
 
     // if player chooses paper
     if(playerchoice == 1 && computerDecision == 2){
-        
         computerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
     };
     if(playerchoice == 1 && computerDecision == 0){
-        
         playerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     if(playerchoice == 1 && computerDecision == 1){
-        
         console.log("Tie");
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     
     // if player chooses scissors
     if(playerchoice == 2 && computerDecision == 0){
-        
         computerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     if(playerchoice == 2 && computerDecision == 1){
-        
         playerScore +=1 ;
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
     if(playerchoice == 2 && computerDecision == 2){
-        
         console.log("Tie");
         console.log("player score: " + playerScore);
         console.log("computer score: " + computerScore);
-        
-
     };
-    
-    
     
     scoreCheck();
         
